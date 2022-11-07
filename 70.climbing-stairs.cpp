a39a44b44415415353141c21554d79c969ce4330
fxxma <54296278+fxxma@users.noreply.github.com>
2022-11-06 19:35:28 -0500

    yo

---
+++ 70.climbing-stairs.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=70 lang=cpp
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        double sum = 0;
+        for(int i=0 ; i <= n/2; i++) sum += combo(n-i,i);
+        return sum;
+    }
+
+    double combo(int x , int y){
+        return fact(x) / (fact(y) * fact(x - y));
+    }
+
+    double fact(int b) {
+        if (b == 0 || b == 1){
+            return 1;
+        }else{
+            return b * fact(b - 1);
+        }
+    }
+    
+};
+// @lc code=end
+
